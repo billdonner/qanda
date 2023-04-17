da2fc5c0a6dc0b2920997da4d41c10bb1c3a5ec6
Bill Donner <billdonner@gmail.com>
2023-04-17 00:04:59 -0500

    Initial Commit

---
+++ qanda/qandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  qandaApp.swift
+//  qanda
+//
+//  Created by bill donner on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct qandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
