187adbf7bf0173aea709b53dd9d57dbf59ed88ed
Bill Donner <billdonner@gmail.com>
2023-04-29 20:44:02 -0500

    new validator command line program

--- qanda/ForChatGPT.swift
+++ qanda/ForChatGPT.swift
@@ -1,24 +1,24 @@
 import SwiftUI
 
-struct Challenge : Hashable, Codable {
-  internal init(question: String, answers: [String], correctAnswer: Int, explanation: [String], article: String? = nil, image: String? = nil) {
-    self.question = question
-    self.answers = answers
-    self.correctAnswer = correctAnswer
-    self.explanation = explanation
-    self.article = article
-    self.image = image
-  }
-  
+//PROMPT consider the following structure
+struct Challenge :Codable,Equatable,Hashable {
+  let id : String
   let question: String
+  let topic: String
+  let hint:String // a hint to show if the user needs help
   let answers: [String]
-  let correctAnswer: Int // index into answers of the single correct answer
+  let answer: String // which answer is correct
   let explanation: [String] // reasoning behind the correctAnswer
-  let article: String? // URL of article about the correct Answer
-  let image:String? // URL of image of correct Answer
+  let article: String // URL of article about the correct Answer
+  let image:String // URL of image of correct Answer
 }
+let id = "wld date: april 29,2003 9:52AM"
+// generate THREE challenges as an array of JSON about "Fun Fish Facts"
+//let json_challenges = [
 
 struct GameData : Codable, Hashable,Identifiable {
+
+  
   internal init(subject: String, challenges: [Challenge]) {
     self.subject = subject
     self.challenges = challenges //.shuffled()  //randomize
--- validator/main.swift
+++ validator/main.swift
@@ -1,51 +1,83 @@
 import Foundation
 
-//PROMPT consider the following structure
 struct Challenge :Codable {
- let id : String
+  let id : String
   let question: String
-  let question_number: Int
   let topic: String
   let hint:String // a hint to show if the user needs help
   let answers: [String]
-  let correctAnswer: Int // index into answers of the single correct answer
+  let answer: String // which answer is correct
   let explanation: [String] // reasoning behind the correctAnswer
   let article: String // URL of article about the correct Answer
   let image:String // URL of image of correct Answer
 }
-let id = "wld date: april 29,2003 9:52AM"
 // generate a swift command line program to read and count byes in JSON encoded Challenge arrays
 
 
 
 func main() {
   var count : Int = 0
+  var bytesRead : Int = 0
+  var topics: [String:Int] = [:]
+  var dupeQustions: [String:Int] = [:]
+  
   // Get the list of URLs from the command line arguments.
   let urls = CommandLine.arguments[1...]
-
+  
   // Iterate over the URLs and count the bytes read at each URL.
   for url in urls {
     // Get the data from the URL.
     
-    guard let u = URL(string:url), let data = try? Data(contentsOf: u) else { //tweaked
+    guard let u = URL(string:url) else {
       print("Cant read url \(url)")
       continue
     }
-
-    // Decode the data, which means converting data to Swift objects.
     do {
-      let challenges = try JSONDecoder().decode([Challenge].self, from: data)
-      count = challenges.count
+      let data = try Data(contentsOf: u)
+      bytesRead = data.count
+      
+      // Decode the data, which means converting data to Swift objects.
+      do {
+        let challenges = try JSONDecoder().decode([Challenge].self, from: data)
+        count = challenges.count
+        for challenge in challenges {
+          let key = challenge.topic
+          if let topic =  topics [key] {
+            topics [key] = topic + 1
+          } else {
+            topics [key ] = 1 // a new one
+          }
+          let qkey = challenge.question
+          if let q =  dupeQustions [qkey] {
+            dupeQustions [qkey] = q + 1
+          } else {
+            dupeQustions [qkey ] = 1 // a new one
+          }
+        }
+        // At the end of each url, Print the bytes read and topics
+        print("Read \(url) - \(bytesRead) bytes, \(count) challenges")
+        
+      }
+      catch {
+        print("Could not decode \(u)", error)
+      }
     }
     catch {
-      print("Could not decode \(u)")
+      print("Can't read contents of \(url)" )
+      continue
     }
-
-    // Count the bytes read.
-    let bytesRead = data.count
-
-    // Print the bytes read.
-    print("Read \(bytesRead) bytes , \(count) challenges from \(url)")
+    
+  } // topics
+  for (_, key_value) in topics.enumerated() {
+    let (key,value) = key_value
+    print("Topic - \(key), \(value) challenges")
+  }
+  // duplicates
+  for (_, key_value) in  dupeQustions.enumerated() {
+    let (key,value) = key_value
+    if value > 1 {
+      print("Duplicate Question - \(key), \(value) dupes")
+    }
   }
 }
 
